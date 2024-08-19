d5a641e1925b24e27776c1d7c729722ce494c4c5
SanderJalles <sanderjalles@hotmail.com>
2024-08-19 11:19:55 -0300

    adicionando inserts separados para cada processo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, send_file, session, send_from_direct
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__, static_folder='.', template_folder='.')
 app.secret_key = 'sua_chave_secreta'  # Necessário para usar sessões
@@ -55,8 +56,14 @@ def upload():
             np.random.shuffle(responsaveis)
             df_filtrado['responsavel'] = responsaveis  # Criando a coluna 'responsavel'
 
-            # Salvando os novos dados no banco de dados
-            df_filtrado.to_sql('processos_distribuidos', engine, if_exists='append', index=False)
+            # Fazendo um insert separado para cada processo
+            for _, row in df_filtrado.iterrows():
+                try:
+                    # Inserindo o processo no banco de dados
+                    row.to_frame().T.to_sql('processos_distribuidos', engine, if_exists='append', index=False)
+                except IntegrityError:
+                    # Lida com a duplicidade e continua com os próximos processos
+                    print(f"Processo {row['númeroprocesso']} já existe no banco de dados. Ignorando.")
 
             # Consultando todos os dados da tabela após a inserção
             df_final = pd.read_sql('SELECT * FROM processos_distribuidos', engine)
